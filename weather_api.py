b7bacf66f6ac13d04433efcf34da2cd822dd089c
TheEzio5 <theezio5a@gmail.com>
2026-07-11 20:01:18 +0200

    Display current weather information

--- weather_api.py
+++ weather_api.py
@@ -9,7 +9,11 @@ class WeatherAPI:
 
     def get_weather(self, city):
         city = city.lower()
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric"
         response = requests.get(url)
         print(response.status_code)
-        print(response.text)
+        weather = response.json()
+
+        print(f'City name: {weather["name"]}')
+        print(f'{weather["main"]["temp"]} °C')
+        print(f'Description: {weather["weather"][0]["description"]}')
